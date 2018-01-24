6ab88536199fafa4f3b12c04f60c9c895186e235
Alcides Goldoni Jr <agoldonijr@gmail.com>
2018-01-24 20:28:08 -0200

    Finalizado exer 1

--- CursoShellScript/listas/cap5/exer1.sh
+++ CursoShellScript/listas/cap5/exer1.sh
@@ -4,20 +4,21 @@ MIN_UID=$(grep "^UID_MIN" /etc/login.defs | tr -s "\t" | cut -f2)
 MAX_UID=$(grep "^UID_MAX" /etc/login.defs | tr -s "\t" | cut -f2)
 
 OLDIFS=$IFS
-IFS='\n'
+IFS=$'\n'
 
 #cabecalho
-echo -e "USUARIO\t\tUID\t\tDIR HOME\t\tNOME"
+echo -e "USUARIO\t\tUID\t\tDIR HOME\t\tNOME\t\tESPACO USADO"
 
 for i in $(cat /etc/passwd)
 do
 	USERID=$(echo $i | cut -d":" -f3)
-	if [ $USERID -ge $MIN_UID -a $USERID -le $MAX_UID ];
+	if [ "$USERID" -ge "$MIN_UID" -a "$USERID" -le "$MAX_UID" ]
 	then
 		USER=$(echo $i | cut -d":" -f1)
-		USERDESC=$(echo $i  cut -d":" -f5 | tr -d ',')
-		USERHOME=$(echo $i  cut -d":" -f6)
-		echo -e "$USER\t\t$USERID\t\t$USERHOME\t\t$USERDESC"
+		USERDESC=$(echo $i | cut -d":" -f5 | tr -d ',')
+		USERHOME=$(echo $i | cut -d":" -f6)
+		TAMHOME=$(du -sh /home/$USER | tr -d "/home/$USER")
+		echo -e "$USER\t\t$USERID\t\t$USERHOME\t\t$USERDESC\t\t$TAMHOME"
 	fi
 done
 
