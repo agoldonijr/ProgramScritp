7ae76eaef421deb5e239cbb1f82b46472ff81c46
goldoni <agoldonijr@gmail.com>
2018-02-08 23:50:38 -0200

    script que procura pacotes em uma maquina padronizada

---
+++ procuraPacote.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###############################################################################
+# Esse script procura os pacotes faltantes de uma maquina baseada numa maquina
+# que esteja padrão. Ele recebe os arquivos texto com os pacotes das maquians
+# que podem ser gerados atraves de comando como dpkg --get-selections para
+# distribuicoes Debia like
+#
+# Utilizacao:
+# ./procuraPacote.sh <arquivoPadrao> <arquivoParaBuscar>
+###############################################################################
+PADRAO=$1
+BUSCAR=$2
+
+echo Pacotes nao encontrados:
+#for i in $(cat pacotesMinime)
+for i in $(cat $PADRAO)
+do
+	grep $i $BUSCAR> /dev/null
+	if [ $? -eq 1 ]
+	then
+		echo $i 
+	fi
+done
