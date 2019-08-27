c3e4c96323f32ebde9409f0a0967c6578a0318c1
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2019-08-27 13:24:39 -0300

    Scripts que estao sem categoria definida

--- diversos/cluster-bunda.sh
+++ diversos/cluster-bunda.sh
@@ -1,3 +1,10 @@
+# Copyright (C) 2019 Alcides Goldoni Junior <agoldonijr@gmail.com>
+# Copyright (C) 2019 Alcides Goldoni Junior <goldoni@ggaunicamp.com>
+
+# Esse script foi feito com ajuda do Ian Liu Rodrigues
+# https://github.com/ianliu
+
+
 #!/bin/bash
 #PBS -l nodes=1:ppn=2
 #PBS -N cluster-bunda
