1bd6cb627f78fa8825bf5c3195cf94d4df0c7719
Alcides Goldoni Junior <goldoni@ggaunicamp.com>
2019-08-27 13:26:36 -0300

    Scripts de padronizacao de Ubuntu para o HPG

--- padronizacao/padronizacaoUbuntu.sh
+++ padronizacao/padronizacaoUbuntu.sh
@@ -1,3 +1,6 @@
+# Copyright (C) 2019 Alcides Goldoni Junior <agoldonijr@gmail.com>
+# Copyright (C) 2019 Alcides Goldoni Junior <goldoni@ggaunicamp.com>
+
 #!/bin/bash
 
 echo "Atualizando repositorios"
